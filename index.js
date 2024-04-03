6b00ce3564bfe21c83723ada47b347374bc701db
salmandanish03 <salmandanish03@gmail.com>
2024-04-03 12:27:51 +0500

    create number guessing game

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-// Computer will generate a random number
-// User input for guessing the number
-// compare user input with computer generated number and show the result
-const randomNumber = 17;
+// Computer will generate a random number (done)
+// User input for guessing the number (done)
+// compare user input with computer generated number and show the result (done)
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+// console.log(randomNumber);
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number: ",
+        message: "Please guess a number b/t 1-10: ",
     }
 ]);
 if (answers.userGuessedNumber === randomNumber) {
-    console.log("You guessed the correct number");
+    console.log("Congratulations! You guessed the correct number");
 }
 else {
     console.log("You guessed the wrong number");
--- index.ts
+++ index.ts
@@ -1,21 +1,25 @@
+#! /usr/bin/env node
+
 import inquirer from "inquirer";
 
-// Computer will generate a random number
-// User input for guessing the number
-// compare user input with computer generated number and show the result
+// Computer will generate a random number (done)
+// User input for guessing the number (done)
+// compare user input with computer generated number and show the result (done)
 
-const randomNumber = 17;
+const randomNumber = Math.floor(Math.random() * 10 +1);
+// console.log(randomNumber);
+
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number: ",
+        message: "Please guess a number b/t 1-10: ",
         
     }
 ]);
 
 if (answers.userGuessedNumber === randomNumber){
-    console.log("You guessed the correct number");
+    console.log("Congratulations! You guessed the correct number");
 }else {
 
     console.log("You guessed the wrong number");
